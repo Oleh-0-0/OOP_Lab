80622bbe5bcb68a03e7259e7a21bcc2bdb164d89
Oleh <frayuk333oleg@gmail.com>
2026-03-12 09:57:40 +0200

    Lab3: added copy/move constructors, static field, operator overloading

--- Lab_2/Student.h
+++ Lab_2/Student.h
@@ -5,13 +5,29 @@ using namespace std;
 class Student
 {
 private:
+
 	string name;
 	string surname;
 	int age;
+
+	static int studentCount;
+
 public:
+
 	Student();
 	Student(string name, string surname, int age);
+	Student(const Student& other);
+	Student(Student&& other);
 	~Student();
+
 	void display() const;
+	void setAge(int age);
+
+	static int getCount();
+
+	Student operator+(const Student& other);
+
+	friend ostream& operator<<(ostream& os, const Student& s);
+	friend istream& operator>>(istream& is, Student& s);
 };
 #endif
