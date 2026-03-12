80ceb8e84572688e60ab48edf99d7788942b9f06
Oleh <frayuk333oleg@gmail.com>
2026-03-12 09:58:32 +0200

    Lab3: implemented copy/move constructors, static field, operator overloading and stream operators

--- Lab_2/Student.cpp
+++ Lab_2/Student.cpp
@@ -1,18 +1,65 @@
 #include "Student.h"
 #include <iostream>
+
 using namespace std;
 
-Student::Student(string name, string surname, int age)
-	: name(name), surname(surname), age(age) {}
+int Student::studentCount = 0;
 
-Student::Student() : Student("Oleh", "Frayuk", 18 ) {}
+Student::Student() : Student("Oleh", "Frayuk", 18) {}
+
+Student::Student(string name, string surname, int age)
+    : name(name), surname(surname), age(age) {
+    studentCount++;
+}
+
+Student::Student(const Student& other)
+    : name(other.name), surname(other.surname), age(other.age) 
+{
+    cout << "Student copied\n";
+}
+
+
+Student::Student(Student&& other)
+    : name(move(other.name)), surname(move(other.surname)),
+    age(other.age)
+{
+    cout << "Student moved\n"
+}
 
 Student::~Student()
 {
-	cout << "Sudent deleate\n";
+    studentCount--;
+    cout << "Student delete\n";
 }
 
 void Student::display() const
 {
-	cout << "Name " << name << ", Surname " << surname << ", Age " << age << endl;
+    cout << "Name: " << name << ", Surname: " << surname << ", Age: " << age << endl;
+}
+
+void Student::setAge(int age) 
+{
+    this->age = age;
+}
+
+int Student::getCount()
+{
+    return studentCount;
+}
+
+Student Student::operator+(const Student& other)
+{
+    return Student(name + "&" + other.name, surname, age);
+}
+
+ostream& operator<<(ostream& os, const Student& s)
+{
+    os << s.name << " " << s.surname "" << s.age;
+    return os;
+}
+
+istream& operator>>(istream& is, Student& s) 
+{
+    is >> s.name >> s.surname >> s.age;
+    return is;
 }
