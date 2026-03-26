beb7f0b67403772bdc33f437ef0ce58b0992060d
Oleh <frayuk333oleg@gmail.com>
2026-03-26 10:04:59 +0200

    Add inheritance in the Schedule class

--- Lab_2/Schedule.cpp
+++ Lab_2/Schedule.cpp
@@ -1,19 +1,62 @@
 #include "Schedule.h"
 #include <iostream>
-using namespace std;
 
-Schedule::Schedule(string day, string time, int auditorium)
-	: day(day), time(time), auditorium(auditorium) {
+Schedule::Schedule() : Schedule("Monday", "08:00", "101") {}
+
+Schedule::Schedule(string day, string time, string room)
+    : day(day), time(time), room(room), course(nullptr) {
 }
 
-Schedule::Schedule() : Schedule("Monday", "8:20", 212 ) {}
-
-Schedule::~Schedule()
-{
-	cout << "Schedule delete\n";
+Schedule::Schedule(string day, string time, string room, Course* course)
+    : day(day), time(time), room(room) {
+    if (course)
+        this->course = new Course(*course);
+    else
+        this->course = nullptr;
 }
 
-void Schedule::display() const
-{
-	cout << "Day " << day << ", Time " << time << ", Auditorium " << auditorium << endl;
+Schedule::Schedule(const Schedule& other)
+    : day(other.day), time(other.time), room(other.room) {
+    if (other.course)
+        course = new Course(*other.course);
+    else
+        course = nullptr;
+}
+
+Schedule& Schedule::operator=(const Schedule& other) {
+    if (this != &other) {
+        day = other.day;
+        time = other.time;
+        room = other.room;
+
+        delete course;
+
+        if (other.course)
+            course = new Course(*other.course);
+        else
+            course = nullptr;
+    }
+    return *this;
+}
+
+Schedule::~Schedule() {
+    delete course;
+    cout << "Schedule destroyed" << endl;
+}
+
+void Schedule::display() const {
+    cout << "Day: " << day << ", Time: " << time  << ", Room: " << room << endl;
+
+    if (course)
+        course->display();
+}
+
+ExamSchedule::ExamSchedule(string day, string time, string room,
+    Course* course, string examType)
+    : Schedule(day, time, room, course), examType(examType) {
+}
+
+void ExamSchedule::display() const {
+    Schedule::display();
+    cout << "Exam type: " << examType << endl;
 }
--- Lab_2/Schedule.h
+++ Lab_2/Schedule.h
@@ -1,17 +1,41 @@
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
+
 #include <string>
+#include <iostream>
+#include "Course.h"
+
 using namespace std;
-class Schedule
-{
+
+class Schedule {
 private:
-	string day;
-	string time;
-	int auditorium;
+    string day;
+    string time;
+    string room;
+    Course* course;
+
 public:
-	Schedule();
-	Schedule(string day, string time, int auditorium);
-	~Schedule();
-	void display() const;
+    Schedule();
+    Schedule(string day, string time, string room);
+    Schedule(string day, string time, string room, Course* course);
+
+    Schedule(const Schedule& other);
+    Schedule& operator=(const Schedule& other);
+
+    virtual ~Schedule();
+
+    virtual void display() const;
 };
+
+class ExamSchedule : public Schedule {
+private:
+    string examType;
+
+public:
+    ExamSchedule(string day, string time, string room,
+        Course* course, string examType);
+
+    void display() const override;
+};
+
 #endif
